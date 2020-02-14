3dff217201b26821895a906bba22e2c4a52d22b7
zhechao <zhechao@ksu.edu>
2020-02-14 10:32:53 -0500

    Merge branch 'master' of /Users/ErnestWong/Desktop/csci_6511/CSCI6511 with conflicts.

--- Proj1/informedSearch.py
+++ Proj1/informedSearch.py
@@ -1,23 +1,27 @@
 import math
-import heapq
+
 
 # main method for A*(informed search), which will take the index of start point(str),the index of end point(str),
 # and the nodes map(dict)
 def a_star(start_index, goal_index, nodes):
     close_list = {}  # list for nodes which have already evaluated
-    open_list = []
+    open_list = {}  # list for noes which need to be evaluated
+    # open_list = PriorityQueue()
     h_values = {}
     g_values = {}
     f_values = {}
+    # start_node = nodes[start_index]
     g_values[start_index] = 0
     h_values[start_index] = int(heuristic(start_index, goal_index, nodes))
     f_values[start_index] = g_values[start_index] + h_values[start_index]
-
-    heapq.heappush(open_list, (f_values[start_index], start_index))
+    open_list[(start_index, f_values[start_index])] = 1
+    # open_list.put((f_values[start_index], start_index))
     close_list[start_index] = None
     while open_list:
-        current_pair = heapq.heappop(open_list)
-        current_index = current_pair[1]
+        current_pair = get_lowest_f(open_list)
+        current_index = current_pair[0]
+        del open_list[current_pair]
+        # current_index = open_list.get()[1]
         neighbors = nodes[current_index].edges.keys()
         for neighbor in neighbors:
             tentative_g = g_values[current_index] + int(nodes[current_index].edges[neighbor])
@@ -25,10 +29,12 @@ def a_star(start_index, goal_index, nodes):
                 g_values[neighbor] = tentative_g
                 h_values[neighbor] = int(heuristic(neighbor, goal_index, nodes))
                 f_values[neighbor] = g_values[neighbor] + h_values[neighbor]
-                heapq.heappush(open_list, (f_values[neighbor], neighbor))
+                open_list[(neighbor, f_values[neighbor])] = 1
+                # open_list.put((f_values[neighbor], neighbor))
                 close_list[neighbor] = current_index
         # arrive the goal node, stop searching
         if current_index == goal_index:
+            # if current_index not in open_list.keys():
             if not check_node_existence(current_index, open_list):
                 return g_values[current_index], close_list
 
@@ -47,10 +53,21 @@ def heuristic(current_index, goal_index, nodes):
     return distance
 
 
-# check whether there is a pair contains goal node in the list
-def check_node_existence(target_index, open_list):
-    for pair in open_list:
-        node_index = pair[1]
+# return the node with the lowest f in the open list
+def get_lowest_f(open_list):
+    lowest_f = math.inf
+    return_pair = None
+    for node_pair in open_list.keys():
+        pair_f = node_pair[1]
+        if pair_f < lowest_f:
+            lowest_f = pair_f
+            return_pair = node_pair
+    return return_pair
+
+
+def check_node_existence(target_index,open_list):
+    for pair in open_list.keys():
+        node_index = pair[0]
         if node_index == target_index:
             return True
     return False
